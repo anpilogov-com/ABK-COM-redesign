1a2439cefccbcc2a8dcfe920dc4aead2ec22a380
Артём Олегович <com.anpilogov.a.o@gmail.com>
2021-09-14 18:19:17 +0300

    Menu is completed

---
+++ src/js/index.js
@@ -0,0 +1,73 @@
+let isMobile = {
+   Android: () => {
+      return navigator.userAgent.match(/Android/i);
+   },
+   BlackBerry: () => {
+      return navigator.userAgent.match(/BlackBerry/i);
+   },
+   iOS: () => {
+      return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+   },
+   Opera: () => {
+      return navigator.userAgent.match(/Opera Mini/i);
+   },
+   Windows: () => {
+      return navigator.userAgent.match(/IEMobile/i);
+   },
+   any: () => {
+      return (
+         isMobile.Android() ||
+         isMobile.BlackBerry() ||
+         isMobile.iOS() ||
+         isMobile.Opera() ||
+         isMobile.Windows()
+      );
+   },
+};
+
+let body = document.querySelector("body");
+
+if (isMobile.any()) {
+   body.classList.add("touch");
+
+   let arrow = document.querySelectorAll(".arrow");
+
+   let burger = document.querySelector(".header__burger");
+
+   for (let index = 0; index < arrow.length; index++) {
+      let thisLink = arrow[index].previousElementSibling;
+      let subMenu = arrow[index].nextElementSibling;
+      let thisArrow = arrow[index];
+
+      arrow[index].addEventListener("click", () => {
+         collapsed(index, arrow);
+         subMenu.classList.toggle("open");
+         thisArrow.classList.toggle("arrow-active");
+      });
+
+      thisLink.classList.add("parent");
+
+      thisLink.addEventListener("click", () => {
+         collapsed(index, arrow);
+         subMenu.classList.toggle("open");
+         thisArrow.classList.toggle("arrow-active");
+      });
+
+      burger.addEventListener("click", () => {
+         if (!burger.classList.contains("active")) {
+            collapsed(index, arrow);
+         }
+      });
+   }
+} else {
+   body.classList.add("mouse");
+}
+
+function collapsed(index, arrow) {
+   for (let i = 0; i < arrow.length; i++) {
+      if (i != index) {
+         arrow[i].nextElementSibling.classList.remove("open");
+         arrow[i].classList.remove("arrow-active");
+      }
+   }
+}
